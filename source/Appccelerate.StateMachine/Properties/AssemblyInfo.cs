899928940cfa8601a966a6eafed6610918a5b984
Urs Enzler <urs.enzler@gmail.com>
2014-04-03 13:06:33 +0200

    updated all references and copyright

--- source/Appccelerate.StateMachine/Properties/AssemblyInfo.cs
+++ source/Appccelerate.StateMachine/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿//-------------------------------------------------------------------------------
 // <copyright file="AssemblyInfo.cs" company="Appccelerate">
-//   Copyright (c) 2008-2013
+//   Copyright (c) 2008-2014
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
